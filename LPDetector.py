2aaadf5c819579bf8cfb8a87bb02d515de2d221e
Michelle Sit <msit@wellesley.edu>
2015-02-04 01:33:18 -0500

    basic show image

--- LPDetector.py
+++ LPDetector.py
@@ -12,3 +12,21 @@ import numpy
 import cv2
 import sys
 
+
+#video_capture = cv2.VideoCapture('./MA_plate.jpg') #Will be used for video capture
+video_capture = cv2.imread('IMG_0378.jpg')
+
+while True:
+	#ret, frame = video_capture.read() # will be used for video capture
+
+
+
+	#displays frames
+	#cv2.imshow('Video', frame)
+	cv2.imshow('picture', video_capture)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+video_capture.release()
+cv2.destroyAllWindows()
